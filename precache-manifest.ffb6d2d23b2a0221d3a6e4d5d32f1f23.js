a255d6950a88789889e144de8debb4e4e12d427a
ZHIMIN ZHNAG <77595103@zhiminzhang.com>
2019-02-25 13:21:19 +0800

    updates

---
+++ precache-manifest.ffb6d2d23b2a0221d3a6e4d5d32f1f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "783a4ea2c3fba3d8c909",
+    "url": "/static/css/main.ebbd45b2.chunk.css"
+  },
+  {
+    "revision": "783a4ea2c3fba3d8c909",
+    "url": "/static/js/main.783a4ea2.chunk.js"
+  },
+  {
+    "revision": "6e912f5239d9a37cedae",
+    "url": "/static/css/1.8fa9635a.chunk.css"
+  },
+  {
+    "revision": "6e912f5239d9a37cedae",
+    "url": "/static/js/1.6e912f52.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7460c5c8842b059438d7a143c33a588d",
+    "url": "/static/media/bg.7460c5c8.jpg"
+  },
+  {
+    "revision": "24852b07ea255d68922f3656962abcaf",
+    "url": "/index.html"
+  }
+];
